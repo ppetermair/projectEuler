21d3c58a984073bdccf735c6e4e4d1ce446f166a
ppetermair <github@petermair.at>
2016-02-28 12:11:50 +0100

    python problem #3

---
+++ python/003.py
@@ -0,0 +1,14 @@
+def primesFactors(n):
+    primfac = []
+    d = 2
+    while d*d <= n:
+        while (n % d) == 0:
+            primfac.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+       primfac.append(n)
+    return primfac
+
+print (primesFactors(600851475143)[-1])
+
